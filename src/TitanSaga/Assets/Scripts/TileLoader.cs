47b6673dcbb8b210f333c649d2118222f715ed83
buzzler <buzzler@unityscene.com>
2016-03-04 03:49:55 +0900

    update..s

--- src/TitanSaga/Assets/Scripts/TileLoader.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class TileLoader : MonoBehaviour {
-	void Awake() {
-		GameObject.DontDestroyOnLoad(gameObject);
-	}
-}
---
+++ src/TitanSaga/Assets/Scripts/TileManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using TileLib;
+
+public class TileManager : MonoBehaviour {
+	public	TextAsset	asset;
+	private	TileConfig	_config;
+
+	void Awake() {
+		GameObject.DontDestroyOnLoad(gameObject);
+		_config = TileConfig.LoadFromText(asset.text);
+	}
+
+	public	TileConfig config {
+		get {
+			return _config;
+		}
+	}
+
+	public	void LoadMap(string id) {
+		TileMap map = _config.tutorial.FindItem(id);
+		if (map != null) {
+			LoadMap(map);
+		}
+	}
+
+	public	void LoadMap(TileMap map) {
+		// TODO : Create Terrain
+	}
+}
